9368a83ac2dde7dd8a70e15e0b15efee3b83ca2e
Eladio Lima Magalhães Júnior <eladiojunior_bsi@hotmail.com>
2019-08-24 22:30:45 -0300

    Inclusão do projeto e a documentação.

---
+++ SGA.ModuloGestaoNormas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SGA.ModuloGestaoNormas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
