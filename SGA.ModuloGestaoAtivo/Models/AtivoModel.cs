c1c35dafc9d2d4e8113e999717779e7bf21b5841
Eladio Lima Magalhães Júnior <eladiojunior_bsi@hotmail.com>
2019-09-06 08:52:28 -0300

    Atualizar repositorio.

---
+++ SGA.ModuloGestaoAtivo/Models/AtivoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGA.ModuloGestaoAtivo.Models
+{
+    public class AtivoModel
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public int QtdDiasPeriodoManutencao { get; set; }
+        public DateTime? UltimaManutencao { get; set; }
+    }
+}
